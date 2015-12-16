6ca72d5297c7bf01fe34a5ac899b97f6f9a0935e
Harvard Young <harvard.young@gmail.com>
2015-12-16 23:52:58 +0000

    updated frequency information in directories

--- Directory.js
+++ Directory.js
@@ -11,6 +11,10 @@ let directoryModel = {
       required: true,
       url: true
     },
+    module: {
+      type: 'string',
+      required: true
+    },
     country: {
       type: 'string',
       required: true,
@@ -44,12 +48,87 @@ let directoryModel = {
         'year',
         'years'
       ],
-      defaultsTo: 'month'
+      defaultsTo: 'week'
     },
     frequencyInterval: {
       type: 'integer',
       integer: true,
       defaultsTo: 1
+    },
+
+    pendingFrequency: {
+      type: 'string',
+      required: true,
+      enum: [
+        'minute',
+        'minutes',
+        'hour',
+        'hours',
+        'day',
+        'days',
+        'week',
+        'weeks',
+        'month',
+        'months',
+        'year',
+        'years'
+      ],
+      defaultsTo: 'days'
+    },
+    pendingFrequencyInterval: {
+      type: 'integer',
+      integer: true,
+      defaultsTo: 2
+    },
+
+    foundFrequency: {
+      type: 'string',
+      required: true,
+      enum: [
+        'minute',
+        'minutes',
+        'hour',
+        'hours',
+        'day',
+        'days',
+        'week',
+        'weeks',
+        'month',
+        'months',
+        'year',
+        'years'
+      ],
+      defaultsTo: 'month'
+    },
+    foundFrequencyInterval: {
+      type: 'integer',
+      integer: true,
+      defaultsTo: 1
+    },
+
+    successFrequency: {
+      type: 'string',
+      required: true,
+      enum: [
+        'minute',
+        'minutes',
+        'hour',
+        'hours',
+        'day',
+        'days',
+        'week',
+        'weeks',
+        'month',
+        'months',
+        'year',
+        'years'
+      ],
+      defaultsTo: 'month'
+    },
+    successFrequencyInterval: {
+      type: 'integer',
+      integer: true,
+      defaultsTo: 2
     },
 
     reportingList: {
