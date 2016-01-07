66ea2985810924e5bf854a462acb36f68b75a5e1
Harvard Young <harvard.young@gmail.com>
2016-01-07 16:35:30 +0000

    new added legacy admin user

---
+++ LegacyUser.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+	connection: 'mysql',
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+	tableName: 'adminusers',
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      columnName: 'user_id'
+    },
+    partner: {
+      type: 'string',
+      columnName: 'username'
+    },
+    firstName: {
+      type: 'string',
+      columnName: 'name'
+    },
+    lastName: {
+      type: 'string',
+      columnName: 'last'
+    },
+    password: {
+      type: 'string',
+      columnNmae: 'password'
+    },
+    role: {
+      type: 'string',
+      enum: ['admin','agent','manager','sales','staff'],
+      defaultsTo: 'admin'
+    }
+  }
+};  
+
