7322784dd08470a551bd893b4ce0a86edbe13398
Harvard Young <harvard.young@gmail.com>
2016-02-04 19:05:11 +0000

    added admins for api keys

---
+++ Admins.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var md5 = require('md5');
+
+module.exports = {
+  attributes: {
+    email: {
+      type: 'string',
+      required: true,
+      index: true
+    },
+    apiKey: {
+      type: 'string',
+      required: true,
+      index: true
+    }
+};  
