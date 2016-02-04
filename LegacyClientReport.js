3bb34dc123142bac66251e499079b0b7e4059c84
Harvard Young <harvard.young@gmail.com>
2016-02-04 21:54:16 +0000

    added legacy report data table

---
+++ LegacyClientReport.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+	connection: 'mysql',
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+	tableName: 'v3_report',
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      columnName: 'id'
+    },
+    clientId: {
+      type: 'integer',
+      columnName: 'clientid'
+    },
+    data: {
+      type: 'string',
+      columnName: 'previous'
+    },
+    type: {
+      type: 'string',
+      enum: ['local','directory','ranking','social','reputation','website','citations','competitor','cancelled'],
+      required: true
+    }
+  }
+};  
