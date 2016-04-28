ec244be80751551c49d4055e8b69cb1ae9a67f59
Harvard Young <harvard.young@gmail.com>
2016-04-28 20:53:35 +0000

    added legacy invoices for new reports

---
+++ LegacyInvoice.js
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = {
+	connection: 'mysql',
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+	tableName: 'invoices',
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      columnName: 'invoice_id'
+    },
+    partner: {
+      model: 'legacypartner',
+      columnName: 'partner_id'
+    },
+    datePaid: {
+      type: 'datetime',
+      columnName: 'paid_date'
+    },
+    dateAttempted: {
+      type: 'datetime',
+      columnName: 'attempt_date'
+    },
+    data: {
+      type: 'json'
+    },
+    paid: {
+      type: 'boolean',
+      defaultsTo: false
+    },
+    notes: {
+      type: 'text'
+    },
+
+    totalServices: {
+      type: 'float',
+      required: true,
+      columnName: 'total_services'
+    },
+    totalReporting: {
+      type: 'float',
+      required: true,
+      columnName: 'total_reporting'
+    },
+    totalMaintenance: {
+      type: 'float',
+      required: true,
+      columnName: 'total_maintenance'
+    },
+    total: {
+      type: 'float',
+      required: true,
+      columnName: 'total_grand'
+    },
+    adjustments: {
+      type: 'float'
+    }
+
+  }
+};  
+
