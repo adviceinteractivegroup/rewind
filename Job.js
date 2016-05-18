ea02cddb21d8f2f669cd1bd392a1672697e9883b
Harvard Young <harvard.young@gmail.com>
2016-05-18 23:35:23 +0000

    added job processing

---
+++ Job.js
@@ -0,0 +1,31 @@
+'use strict';
+/**
+* Log.js
+*
+* @description :: TODO: You might write a short summary of how this mo
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+  attributes: {
+    params: {
+      type: 'json',
+      required: true
+    },
+    callback: {
+      type: 'string'
+    },
+    callbackResponse: {
+      type: 'string'
+    },
+    completedAt: {
+      type: 'datetime'
+    },
+    completed: {
+      type: 'boolean',
+      defaultsTo: false,
+      indexed: true
+    }
+  }
+};
+
