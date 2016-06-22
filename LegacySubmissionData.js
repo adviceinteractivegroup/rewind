b99a01d5b0f49c1b8d5454f8978c8946b094231e
Harvard Young <harvard.young@gmail.com>
2016-06-22 00:03:49 +0000

    added legacy submission data model

---
+++ LegacySubmissionData.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+	connection: 'mysql',
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+	tableName: 'submission_data',
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      columnName: 'SUBMISSION_DATA_ID'
+    },
+    directory: {
+      type: 'string',
+      required: true,
+      index: true
+    },
+    clientId: {
+      type: 'integer',
+      required: true,
+      columnName: 'client_id'
+    },
+    createdAt: {
+      type: 'datetime',
+      columnName: 'thedate'
+    },
+    success: {
+      type: 'string',
+      enum: ['false','true'],
+      defaultsTo: 'false'
+    },
+    error: {
+      type: 'string'
+    }
+  }
+};  
+
