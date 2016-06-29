894cb84e655a47ecc6c2b7c4766f0f3905e1df22
Harvard Young <harvard.young@gmail.com>
2016-06-29 17:22:46 +0000

    added legacy orders to the model

---
+++ LegacyOrder.js
@@ -0,0 +1,58 @@
+'use strict';
+
+module.exports = {
+	connection: 'mysql',
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+	tableName: 'orders',
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      columnName: 'id'
+    },
+    client: {
+      model: 'legacyclient',
+      columnName: 'client'
+    },
+    products: {
+      type: 'string',
+      columnName: 'orde'
+    },
+    createdAt: {
+      type: 'datetime',
+      columnName: 'dates'
+    },
+    startedAt: {
+      type: 'datetime',
+      columnName: 'start_date'
+    },
+    publishedAt: {
+      type: 'datetime',
+      columnName: 'publish_date'
+    },
+    isPaid: {
+      type: 'boolean',
+      defaultsTo: false,
+      columnName: 'paid'
+    },
+    isVerified: {
+      type: 'boolean',
+      defaultsTo: false,
+      columnName: 'verified'
+    },
+    isDrip: {
+      type: 'boolean',
+      defaultsTo: false,
+      columnName: 'drip_feed'
+    },
+    chargeId: {
+      type: 'string',
+      columnName: 'charge_id'
+    },
+    schedule: {
+      type: 'json'
+    }
+  }
+};  
+
