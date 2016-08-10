6f457fa61feec4914d35a7bbb72375e36a1afbe2
Harvard Young <harvard.young@gmail.com>
2016-08-10 02:52:03 +0000

    added legacy products and orders products to the models

---
+++ LegacyOrderProduct.js
@@ -0,0 +1,78 @@
+'use strict';
+
+module.exports = {
+	connection: 'mysql',
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+	tableName: 'orders_products',
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      columnName: 'id'
+    },
+    partner: {
+      model: "legacypartner",
+      columnName: 'partner_id'
+    },
+    client: {
+      model: "legacyclient",
+      columnName: 'client_id'
+    },
+    product: {
+      model: "legacyproduct",
+      columnName: 'product_id'
+    },
+    customPackage: {
+      type: 'string',
+      columnName: 'custom_package'
+    },
+
+    price: {
+      type: 'float',
+      columnName: 'price'
+    },
+    priceMaintenance: {
+      type: 'float',
+      columnName: 'price'
+    },
+    priceRenewal: {
+      type: 'float',
+      columnName: 'price'
+    },
+
+    delayMaintenance: {
+      type: 'int',
+      columnName: 'delay_maintenance'
+    },
+    intervalMaintenance: {
+      type: 'int',
+      columnName: 'interval_maintenance'
+    },
+    intervalRenewal: {
+      type: 'int',
+      columnName: 'interval_renewal'
+    },
+
+    periodMaintenance: {
+      type: 'int',
+      columnName: 'period'
+    },
+
+    isDrip: {
+      type: 'string',
+      columnName: 'drip_feed',
+      enum: ['false','true'],
+      defaultsTo: 'false'
+    },
+    isDisabled: {
+      type: 'string',
+      columnName: 'disabled',
+      enum: ['false','true'],
+      defaultsTo: 'false'
+    },
+
+
+  }
+};  
+
---
+++ LegacyProduct.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = {
+	connection: 'mysql',
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+	tableName: 'products',
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      columnName: 'id'
+    },
+    name: {
+      type: 'string',
+      columnName: 'product'
+    },
+    description: {
+      type: 'string'
+    },
+    extra: {
+      type: 'string',
+      columnName: 'description_extra'
+    },
+    priceRetail: {
+      type: 'float',
+      columnName: 'pricea'
+    },
+    priceReseller: {
+      type: 'float',
+      columnName: 'priceb'
+    },
+    maintenanceInterval: {
+      type: 'string',
+      columnName: 'repeat'
+    },
+    productType: {
+      type: 'string',
+      columnName: 'type'
+    },
+    maintenancePriceRetail: {
+      type: 'float',
+      columnName: 'repeat_aprice'
+    },
+    maintenancePriceReseller: {
+      type: 'float',
+      columnName: 'repeat_price'
+    },
+    submissionModule: {
+      type: 'string',
+      columnName: 'submission_module'
+    }
+  }
+};  
+
