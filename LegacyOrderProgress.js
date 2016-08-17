0ac5891f250da419e373a51da1899fd24fd37b27
Harvard Young <harvard.young@gmail.com>
2016-08-17 21:10:01 +0000

    added legacy order progress to stats

---
+++ LegacyOrderProgress.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+	connection: 'mysql',
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+	tableName: 'newsub',
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      columnName: 'id'
+    },
+    order: {
+      model: "legacyorder",
+      columnName: 'orderid'
+    },
+    createdAt: {
+      type: 'datetime',
+      columnName: 'init',
+      defaultsTo: function () { return Date(); }
+    },
+    finishedAt: {
+      type: 'datetime',
+      columnName: 'end',
+      defaultsTo: function () { return Date(); }
+    },
+    status: {
+      type: 'integer'
+    }
+  }
+};  
