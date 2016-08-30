863b15dc7964c68b6af93433a86ddd888da3e259
Harvard Young <harvard.young@gmail.com>
2016-08-30 23:18:37 +0000

    added token stuff for GMB

---
+++ LegacyGmbToken.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+	connection: 'mysql',
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+	tableName: 'gmb_tokens',
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      columnName: 'id'
+    },
+    username: {
+      type: 'string',
+      required: true
+    },
+    password: {
+      type: 'string',
+      required: true
+    },
+    token: {
+      type: 'string',
+      required: true
+    },
+    clients: {
+      type: 'integer',
+      required: true,
+      defaultsTo: 0
+    },
+    createdAt: {
+      date: 'datetime',
+      columnName: 'thedate',
+      defaultsTo: function () { return Date(); }
+    }
+  }
+};  
+
---
+++ LegacyGmbTokenClient.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+	connection: 'mysql',
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+	tableName: 'gmb_tokens_clients',
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      columnName: 'id'
+    },
+    token: {
+      model: 'legacygmbtoken',
+      columnName: 'gmb_token_id'
+    },
+    client: {
+      model: 'legacyclient',
+      columnName: 'client_id'
+    },
+    createdAt: {
+      date: 'datetime',
+      columnName: 'thedate',
+      defaultsTo: function () { return Date(); }
+    }
+  }
+};  
+
