76269eebfe0c12ab75f5b24e7446223f2cc12854
Harvard Young <harvard.young@gmail.com>
2016-11-09 01:29:11 +0000

    stop making statuscode required

--- Link.js
+++ Link.js
@@ -26,8 +26,7 @@ module.exports = {
       defaultsTo: 0
     },
     statusCode: {
-      type: 'integer',
-      required: true
+      type: 'integer'
     },
     invalid: {
       type: 'boolean',
