8b55ec1c2e8ddcc2a1ea9e72be001bde4afc8e58
Harvard Young <harvard.young@gmail.com>
2016-11-10 23:50:09 +0000

    added stat model for daily history

---
+++ Stat.js
@@ -0,0 +1,17 @@
+'use strict';
+/**
+* Log.js
+*
+* @description :: TODO: You might write a short summary of how this mo
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+  attributes: {
+    data: {
+      type: 'json',
+      required: true
+    }
+  }
+};
+
