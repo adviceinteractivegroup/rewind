461d28e035c4f86611e634349709741a6c3673e3
Harvard Young <harvard.young@gmail.com>
2016-11-14 23:19:28 +0000

    aded legacy fulfillment data... i'll miss you newsub... NOT!

---
+++ LegacyFulfillmentData.js
@@ -0,0 +1,111 @@
+'use strict';
+
+module.exports = {
+	connection: 'mysql',
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+	tableName: 'fulfillment_data',
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      columnName: 'id'
+    },
+    partner: {
+      model: "legacypartner",
+      columnName: 'partner_id'
+    },
+    client: {
+      model: "legacyclient",
+      columnName: 'client_id'
+    },
+    order: {
+      model: "legacyorder",
+      columnName: 'order_id'
+    },
+    product: {
+      model: "legacyproduct",
+      columnName: 'product_id'
+    },
+    directory: {
+      type: 'string'
+    },
+    status_initial: {
+      type: 'string',
+      enum: ['not_present','present','claimed'],
+      columnName: 'initial_status'
+    },
+    url: {
+      type: 'string',
+      columnName: 'live_url'
+    },
+    screenshotUrl: {
+      type: 'string',
+      columnName: 'screenshot_url'
+    },
+    status: {
+      type: 'string',
+      enum: ['initial','existing','added_not_claimed','added_claimed','pending','pin_email','pin_phone','pin_postcard','temporary_problem','not_interested','critical_problem','active'],
+      defaultsTo: 'initial'
+    },
+    comment: {
+      type: 'string',
+    },
+    commentCustom: {
+      type: 'string',
+      columnName: 'comment_custom'
+    },
+    dateScreenshot: {
+      type: 'datetime',
+      columnName: 'timestamp_screenshot_url'
+    },
+    username: {
+      type: 'string'
+    },
+    password: {
+      type: 'string'
+    },
+    email: {
+      type: 'string'
+    },
+    recoveryPhone: {
+      type: 'string',
+      columnName: 'recovery_phone'
+    },
+    recoveryEmail: {
+      type: 'string',
+      columnName: 'recovery_email'
+    },
+    fulfillmentUser: {
+      type: 'string',
+      columnName: 'fulfillment_user'
+    },
+
+    dateStarted: {
+      type: 'datetime',
+      columnName: 'date_started'
+    },
+    dateEnded: {
+      type: 'datetime',
+      columnName: 'date_ended'
+    },
+    dateReplaced: {
+      type: 'datetime',
+      columnName: 'date_replaced'
+    },
+    dateCreated: {
+      type: 'datetime',
+      columnName: 'date_created'
+    },
+    dateUpdated: {
+      type: 'datetime',
+      columnName: 'date_updated'
+    },
+    dateVerified: {
+      type: 'datetime',
+      columnName: 'date_verified'
+    },
+
+  }
+};  
+
