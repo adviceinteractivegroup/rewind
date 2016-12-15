da122038d42456c6508803375645751467402156
Harvard Young <harvard.young@gmail.com>
2016-12-15 00:44:46 +0000

    added aclients changelog

---
+++ LegacyClientChange.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+	connection: 'mysql',
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+	tableName: 'aclients_changelog',
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      columnName: 'client_changelog_id'
+    },
+    client: {
+      model: 'legacyclient',
+      columnName: 'client_id',
+      required: true
+    },
+    fields: {
+      type: 'json',
+      required: true
+    },
+    diff: {
+      type: 'json'
+    },
+    createdAt: {
+      type: 'datetime',
+      columnName: 'thedate',
+      required: true
+    }
+  }
+};  
+
