252cf91af4d3a9fb9b0aafc5b813b34095c45310
Harvard Young <harvard.young@gmail.com>
2017-03-22 22:52:25 +0000

    added legacy pricing controller for custom products and packages

---
+++ LegacyPricing.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  attributes: {
+    partner: {
+      model: 'legacypartner',
+      required: true,
+    },
+    name: {
+      type: 'string',
+      required: true,
+    },
+    product: {
+      model: 'legacyproduct',
+    },
+    products: {
+      type: 'json',
+    },
+    price: {
+      type: 'float',
+      required: true,
+    },
+    maintenanceInterval: {
+      type: 'string',
+      enum: ['monthly', 'yearly'],
+      defaultsTo: 'monthly',
+    },
+    delayMaintenance: {
+      type: 'int',
+      defaultsTo: 1,
+    },
+    maintenancePrice: {
+      type: 'float',
+    },
+    isPackage: {
+      type: 'boolean',
+      defaultsTo: false,
+    },
+  },
+};
+
