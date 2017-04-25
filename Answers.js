22bfb8a12005347db488e983da65478c4da29433
Harvard Young <harvard.young@gmail.com>
2017-04-25 18:41:49 +0000

    added knowledge base model

---
+++ Answers.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  attributes: {
+    question: {
+      type: 'string',
+      required: true,
+    },
+    answer: {
+      type: 'string',
+      required: true,
+    },
+    keywords: {
+      type: 'string',
+      required: true,
+    },
+    unhelpful: {
+      type: 'integer',
+      defaultsTo: 0,
+    },
+    helpful: {
+      type: 'integer',
+      defaultsTo: 0,
+    },
+  },
+};
