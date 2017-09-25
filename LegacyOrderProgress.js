3c53c7d186b56db65cd178ad331c26b5c479131b
Harvard Young <harvard.young@gmail.com>
2017-09-25 20:32:10 +0000

    added back order progress for now because of broken stats

---
+++ LegacyOrderProgress.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+	connection: 'mysql',
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+  autoTK: false,
+	tableName: 'newsub',
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      columnName: 'id',
+    },
+    order: {
+      model: 'legacyorder',
+      columnName: 'orderid',
+    },
+    createdAt: {
+      type: 'datetime',
+      columnName: 'init',
+      defaultsTo: function() {
+ return Date();
+},
+    },
+    finishedAt: {
+      type: 'datetime',
+      columnName: 'end',
+      defaultsTo: function() {
+ return Date();
+},
+    },
+    status: {
+      type: 'integer',
+    },
+  },
+};
