ffae91f7fe4f1af50b0e10c6003549bf93727749
Harvard Young <harvard.young@gmail.com>
2019-01-31 20:08:57 +0000

    adding legacy review summary

---
+++ LegacyReviewSummary.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+	connection: 'mysql',
+  autoCreatedAt: false,
+  autoTK: false,
+  autoUpdatedAt: false,
+	tableName: 'review_data',
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      columnName: 'REVIEW_SUMMARY_ID',
+    },
+    client: {
+      model: 'legacyclient',
+      required: true,
+      columnName: 'client_id',
+    },
+    partner: {
+      model: 'legacypartner',
+      required: true,
+      columnName: 'partner_id',
+    },
+    createdAt: {
+      type: 'datetime',
+      columnName: 'date_created',
+      defaultsTo: function() {
+       return new Date();
+      },
+    },
+    data: {
+      type: 'json',
+    }
+  },
+};
+
