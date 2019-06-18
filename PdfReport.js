7a9b0e7affa2a6d6ff4866c391ba817f68859fa5
Pablovem <pablovema@gmail.com>
2019-06-18 17:22:36 -0500

    Update PdfReport Model

--- PdfReport.js
+++ PdfReport.js
@@ -3,33 +3,33 @@
 module.exports = {
   attributes: {
     partner: {
-      model: 'legacypartner',
+      model: 'legacypartner'
     },
-    emails: {
-      type: 'json',
+    reportCycle: {
+      type: 'string'
     },
     pdfReportsBucket: {
       type: 'string',
       required: true,
       unique: true,
-      index: true,
+      index: true
     },
-    pdfZipUrl: {
-      type: 'string',
+    zipUrl: {
+      type: 'string'
     },
-    pdfLastUpdated: {
-      type: 'string',
+    emails: {
+      type: 'json'
     },
     pdfLastRequested: {
-      type: 'string',
+      type: 'string'
     },
     pdfLastUpdated: {
-      type: 'string',
+      type: 'string'
     },
     status: {
       type: 'string',
       enum: ['ready', 'processing', 'suspended', 'done'],
-      defaultsTo: 'ready',
+      defaultsTo: 'ready'
     },
   },
 };
