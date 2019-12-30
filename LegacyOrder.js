1a6df182eec72ee3e0c942e7d46e114491e66a74
Harvard Young <harvard.young@gmail.com>
2019-12-30 21:11:30 +0000

    adding active product count to orders

--- LegacyOrder.js
+++ LegacyOrder.js
@@ -125,6 +125,10 @@ module.exports = {
       obj.products = obj.products.split('|');
       return obj;
     },
+    activeProducts: {
+      type: 'integer',
+      columnName: 'active_products'
+    }
   },
 };
 
