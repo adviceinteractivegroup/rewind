e85d205e6614e30ed8af6c3e8ed5c6b6993a8922
Harvard Young <harvard.young@gmail.com>
2020-04-20 16:12:40 +0000

    legacy upload batch

---
+++ LegacyUploadBatch.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+	connection: 'mysql',
+  autoCreatedAt: false,
+  autoUpdatedAt: false,
+  autoTK: false,
+	tableName: 'aclients_batches',
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      columnName: 'id',
+      autoIncrement: true,
+    },
+    partner: {
+      model: 'legacypartner',
+      columnName: 'partner_id',
+    },
+    filename: {
+      type: 'string',
+      columnName: 'partner_id',
+    },
+    status: {
+      type: 'string'
+    }
+    createdAt: {
+      type: 'datetime',
+      columnName: 'thedate_created',
+      defaultsTo: function() {
+       return new Date();
+      },
+    },
+    updatedAt: {
+      type: 'datetime',
+      columnName: 'thedate_updated',
+    },
+  },
+};
