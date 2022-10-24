931e89fe6101f13de29911ad01bc5650b4bb1cc6
Harvard Young <harvard.young@gmail.com>
2022-10-24 17:28:41 +0000

    legacy status new model

---
+++ LegacyStatus.js
@@ -0,0 +1,31 @@
+'use strict';
+/**
+* Log.js
+*
+* @description :: TODO: You might write a short summary of how this mo
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+	connection: 'mysql',
+	tableName: 'status',
+  attributes: {
+    application: {
+      type: 'string',
+    },
+    message: {
+      type: 'string',
+    },
+    recentChanges: {
+      type: 'string',
+      enum: ['BUG_FIXED', 'BUG_REPORTED','OUTAGE', 'OUTAGE_RESOLVED', 'FEATURE_UPDATE', 'NEW_FEATURE']
+    },
+    version: {
+      type: 'string',
+    },
+    status: {
+      type: 'string',
+      enum: ['OPERATIONAL', 'OUTAGE']
+    },
+  },
+};
