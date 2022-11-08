8dc5833a6c16354efe66b2623625b9cc664d4227
Harvard Young <harvard.young@gmail.com>
2022-11-08 17:03:15 +0000

    renaming partner key model

---
+++ LegacyPartnerKey.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let md5 = require('md5');
+
+module.exports = {
+  connection: 'mysql',
+  tableName: 'partners_keys',
+  attributes: {
+    partner: {
+      model: 'legacypartner',
+      required: true,
+    },
+    hash: {
+      type: 'string',
+    },
+    ipaddr: {
+      type: 'string',
+      required: true,
+    },
+  },
+
+  beforeCreate: function(values, cb) {
+    let hrtime = process.hrtime();
+    values.hash = "tk_" + md5(hrtime + 'warpath forever!');
+    cb();
+  }
+};
