da4d196fb461e3497d161646d059ea99d76f51e1
Harvard Young <harvard.young@gmail.com>
2023-07-16 20:20:59 +0000

    adding google queue

---
+++ LegacyGoogleUpdateQueue.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+	connection: 'mysql',
+  autoCreatedAt: false,
+  autoTK: false,
+  autoUpdatedAt: false,
+	tableName: 'google_update_queue',
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      columnName: 'id',
+      autoIncrement: true,
+    },
+    client: {
+      model: 'legacyclient',
+      columnName: 'client_id',
+    },
+    field: {
+      type: 'string',
+      enum: ['name','category','secondary','address','description','website','phone'],
+    }
+    partner: {
+      model: 'legacypartner',
+      columnName: 'partner_id',
+    },
+    createdAt: {
+      type: 'datetime',
+      defaultsTo: function() {
+       return new Date();
+      },
+    updatedAt: {
+      type: 'datetime',
+      defaultsTo: function() {
+       return new Date();
+      },
+    },
+
+  },
+};
+
